712dcb4d2d4ec4b5e6dd3d3bf29d79a1053f873e
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-08-02 17:03:20 +0400

    countup.js-angular2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { CountUpModule } from 'countup.js-angular2';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    CountUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
